6b44f28259ce4b123ec17d07b4977f82b5b3ee8a
DESKTOP-9FTTE02\Richard Jones <richierichard99@gmail.com>
2018-09-02 20:48:31 +0100

    the Peen

--- GameTemplate.py
+++ GameTemplate.py
@@ -9,31 +9,33 @@ class Player:
         self.shipBoard = np.zeros((10,10))
         self.bombBoard = np.zeros((10,10))
         ### Populating board with ships ###
+        
+    def populate(self, posns, orientations):
         # Carrier
-        if Orientations[0] == 0:
-            self.shipBoard[Posns[0][0]:Posns[0][0]+5,Posns[0][1]] = 1
+        if orientations[0] == 0:
+            self.shipBoard[posns[0][0]:posns[0][0]+5,posns[0][1]] = 1
         else:
-            self.shipBoard[Posns[0][0],Posns[0][1]:Posns[0][1]:Posns[0][1]+5] = 1
+            self.shipBoard[posns[0][0],posns[0][1]:posns[0][1]:posns[0][1]+5] = 1
         # BattleShip
-        if Orientations[1] == 0:
-            self.shipBoard[Posns[1][0]:Posns[1][0]+4,Posns[1][1]] = 1
+        if orientations[1] == 0:
+            self.shipBoard[posns[1][0]:posns[1][0]+4,posns[1][1]] = 1
         else:
-            self.shipBoard[Posns[1][0],Posns[0][1]:Posns[1][1]:Posns[1][1]+4] = 1
+            self.shipBoard[posns[1][0],posns[0][1]:posns[1][1]:posns[1][1]+4] = 1
         # Cruiser
-        if Orientations[2] == 0:
-            self.shipBoard[Posns[2][0]:Posns[1][0]+3,Posns[2][1]] = 1
+        if orientations[2] == 0:
+            self.shipBoard[posns[2][0]:posns[1][0]+3,posns[2][1]] = 1
         else:
-            self.shipBoard[Posns[2][0],Posns[2][1]:Posns[2][1]:Posns[2][1]+3] = 1
+            self.shipBoard[posns[2][0],posns[2][1]:posns[2][1]:posns[2][1]+3] = 1
         # Submarine
-        if Orientations[3] == 0:
-            self.shipBoard[Posns[3][0]:Posns[3][0]+3,Posns[3][1]] = 1
+        if orientations[3] == 0:
+            self.shipBoard[posns[3][0]:posns[3][0]+3,posns[3][1]] = 1
         else:
-            self.shipBoard[Posns[3][0],Posns[3][1]:Posns[3][1]:Posns[3][1]+3] = 1
+            self.shipBoard[posns[3][0],posns[3][1]:posns[3][1]:posns[3][1]+3] = 1
         # Destroyer
-        if Orientations[4] == 0:
-            self.shipBoard[Posns[4][0]:Posns[4][0]+2,Posns[4][1]] = 1
+        if orientations[4] == 0:
+            self.shipBoard[posns[4][0]:posns[4][0]+2,posns[4][1]] = 1
         else:
-            self.shipBoard[Posns[4][0],Posns[4][1]:Posns[4][1]:Posns[4][1]+2] = 1
+            self.shipBoard[posns[4][0],posns[4][1]:posns[4][1]:posns[4][1]+2] = 1
     def recieveAttack(self, x,y):
         # player recieves an attack from other player at position X
         self.board[x,y] = 2
@@ -46,3 +48,4 @@ class Player:
             self.bombBoard[x,y] = 2
     def ViewBoard(self):
         mp.imshow(self.board,cmap = "hot")
+    def 
