1855ecd453b04363798abaa753d24da2f7ca50ea
DESKTOP-9FTTE02\Richard Jones <richierichard99@gmail.com>
2018-09-04 21:27:14 +0100

    hai

--- PlayerClass.py
+++ PlayerClass.py
@@ -23,15 +23,19 @@ class Ship:
             self.hits = 0
             self.status = "Afloat"
             
-        def hit(self):
+        def hit(self, bombBoard):
             self.hits += 1
             # print("hit")
             if self.hits == self.length:
                 self.status = "Sunk!"
+                for tile in self.tiles:
+                    bombBoard[tile] = 2
                 # print("Sunk!")
             return self.status
-            
-
+        
+        def addTiles(self, tiles):
+            self.tiles = tiles
+        
         def reset(self):
             self.hits = 0
             self.status = "Afloat"
@@ -50,41 +54,51 @@ class player:
         self.Cruiser = Ship(3)
         self.Submarine = Ship(3)
         self.Destroyer = Ship(2)
-        
+                
     def populate(self, tiles):
         for i in range(5):
             self.shipBoard[tiles[0][i][0],tiles[0][i][1]] = 1
+            self.Carrier.addTiles(tiles[0][i][0],tiles[0][i][1])
         for i in range(4):
             self.shipBoard[tiles[1][i][0],tiles[1][i][1]] = 2
+            self.Battleship.addTiles(tiles[1][i][0],tiles[1][i][1])
         for i in range(3):
             self.shipBoard[tiles[2][i][0],tiles[2][i][1]] = 3
+            self.Cruiser(tiles[2][i][0],tiles[2][i][1])
         for i in range(3):
             self.shipBoard[tiles[3][i][0],tiles[3][i][1]] = 4
+            self.Submarine.addTiles(tiles[3][i][0],tiles[3][i][1])
         for i in range(2):
             self.shipBoard[tiles[4][i][0],tiles[4][i][1]] = 5
+            self.Destroyer.addTiles(tiles[4][i][0],tiles[4][i][1])
 
     def recieveAttack(self, x,y):
         # player recieves an attack from other player at position X
         if self.shipBoard[x,y] == 1:
-            self.Carrier.hit()
+            self.Carrier.hit(self.bombBoard)
         if self.shipBoard[x,y] == 2:
-            self.Battleship.hit()
+            self.Battleship.hit(self.bombBoard)
         if self.shipBoard[x,y] == 3:
-            self.Cruiser.hit()
+            self.Cruiser.hit(self.bombBoard)
         if self.shipBoard[x,y] == 4:
-            self.Submarine.hit()
+            self.Submarine.hit(self.bombBoard)
         if self.shipBoard[x,y] == 5:
-            self.Destroyer.hit()
+            self.Destroyer.hit(self.bombBoard)
         self.shipBoard[x,y] = -1
-
+                      
     def sendAttack(self, player2, x, y):
-        if player2.shipBoard[x,y] == 0:
+        if self.bombBoard[x,y] != 0:
+            reward = -100
+        elif player2.shipBoard[x,y] == 0:
             # Miss
             self.bombBoard[x,y] = -1
+            reward = 0
         else:
             # Hit
             self.bombBoard[x,y] = 1
+            reward = 1
         player2.recieveAttack(x,y)
+        return reward
 
     def ViewBoard(self):
         mp.figure()
